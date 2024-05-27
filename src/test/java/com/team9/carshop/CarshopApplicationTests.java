f65ee3699082854cf334877bb8a6f92e158083ba
GyuTae <novico@naver.com>
2024-05-27 22:32:06 +0900

    Chore: 프로젝트 의존성 추가

---
+++ src/test/java/com/team9/carshop/CarshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team9.carshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
