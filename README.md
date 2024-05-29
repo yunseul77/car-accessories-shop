89d7f90d072f0d4ed06595a86b131d020c924c57
[Cloud3] 박규태 <novico@naver.com>
2024-05-29 03:52:47 +0000

    Revert "Merge branch 'feature-BE-setting' into 'main'"
    This reverts merge request !1

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # car-accessories-shop
 
 차량 인테리어 용품 쇼핑몰 프로젝트
-
-### 기술 스택
-- Backend
-    - 
--- src/main/java/com/team9/carshop/CarshopApplication.java
+++ src/main/java/com/team9/carshop/CarshopApplication.java
@@ -2,10 +2,8 @@ package com.team9.carshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class CarshopApplication {
 
 	public static void main(String[] args) {
