8b4530be5cfa488e9dfe65006555f28b58a90fdb
gaeun2593@gmail.com <gaeun2593@gmail.com>
2024-05-29 20:33:23 +0900

    가은님 파트에 대한 커밋 리베이스입니다.
    review setting
    
    review
    
    review set
    
    Update ReviewController.java
    
    Update ReviewDTO.java
    
    Delete ReviewController.java
    
    Delete ReviewDTO.java
    
    Delete ReviewService.java
    
    review 전체적 코드 추가

---
+++ src/main/java/com/team9/carshop/entity/Cart.java
@@ -0,0 +1,32 @@
+package com.team9.carshop.entity;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+}
