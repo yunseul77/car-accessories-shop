f5e51d5e7bfbc8eed222f4d785e60067d84f54bc
[Cloud3] 김가은 <gaeun2593@gmail.com>
2024-06-03 15:54:25 +0000

    Update ReviewController.java

--- src/main/java/com/team9/carshop/controller/ReviewController.java
+++ src/main/java/com/team9/carshop/controller/ReviewController.java
@@ -1,14 +1,1 @@
-package com.team9.carshop.controller;
 
-import com.team9.carshop.service.ReviewService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class ReviewController {
-
-    @GetMapping("/review")
-    public String reviewWriteForm() {
-        return "review";
-    }
-}
