03470d3a0c532692f12590afc42e2a0febe4a07e
[Cloud3] 김가은 <gaeun2593@gmail.com>
2024-06-03 15:56:57 +0000

    Delete ReviewRepository.java

--- src/main/java/com/team9/carshop/repository/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.team9.carshop.repository;
-
-import com.team9.carshop.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-}
