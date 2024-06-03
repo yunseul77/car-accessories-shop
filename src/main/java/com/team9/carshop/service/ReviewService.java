990ce7b0419341eef70cd90a2f2ca1f588404295
[Cloud3] 김가은 <gaeun2593@gmail.com>
2024-06-03 15:57:05 +0000

    Delete ReviewService.java

--- src/main/java/com/team9/carshop/service/ReviewService.java
+++
@@ -1,8 +0,0 @@
-package com.team9.carshop.service;
-
-import com.team9.carshop.repository.ReviewRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ReviewService {
-}
