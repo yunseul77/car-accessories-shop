0f7fa2703b6f5f71c13b0352825e84032806cd99
cloud_track / class_03 / web_project1 / Team06 <engus7303@gmail.com>
2024-06-04 22:06:08 +0900

    item_1

--- src/main/java/com/team9/carshop/repository/CategoryItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.team9.carshop.repository;
-
-import com.team9.carshop.entity.CategoryItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryItemRepository extends JpaRepository<CategoryItem, Long> {
-
-}
