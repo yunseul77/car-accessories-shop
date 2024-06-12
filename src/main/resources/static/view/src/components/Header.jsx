5adb8280e888117d202092fbba4e1ac1b1a62fc2
park <novico@naver.com>
2024-06-12 21:58:02 +0900

    Feat: 리액트 프로젝트 ì»프로젝트 컴포넌트+디ë í í트 컴포넌디렉토리 분류, 컴포넌트 추가

---
+++ src/main/resources/static/view/src/components/Header.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import logo from '../assets/로고.png';
+
+const Header = () => {
+  return (
+      <header className="py-3 mb-4 border-bottom">
+        <div className="container d-flex flex-wrap justify-content-between align-items-center">
+          <a href="/main" className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none" style={{ flex: "0.6" }}>
+            <img src={logo} alt="Logo" style={{ width: "40px", height: "40px", marginRight: "10px" }} />
+            <span className="fs-4">차량용품</span>
+          </a>
+          <select className="form-select" aria-label="Default select example" style={{ flex: "0.4" }}>
+            <option selected>전체</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+            <option value="4">Four</option>
+            <option value="5">Five</option>
+            <option value="6">Six</option>
+          </select>
+          <form className="col-12 col-lg-auto mb-3 mb-lg-0" role="search" style={{ flex: "3" }}>
+            <input type="search" className="form-control" placeholder="상품을 검색하세요." aria-label="Search" />
+          </form>
+          <div className="d-flex gap-2 justify-content-end" style={{ flex: "0.72" }}>
+            <button className="btn btn-primary me-md-2" type="button" style={{ display: "flex", flexDirection: "column", alignItems: "center", background: "none", border: "none" }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16" style={{ color: "black" }}>
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+              </svg>
+              <span style={{ fontSize: "70%", color: "black" }}>마이페이지</span>
+            </button>
+            <button className="btn btn-primary me-md-2" type="button" style={{ display: "flex", flexDirection: "column", alignItems: "center", background: "none", border: "none" }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="black" className="bi bi-cart3" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+              </svg>
+              <span style={{ fontSize: "70%", color: "black" }}>장바구니</span>
+            </button>
+          </div>
+        </div>
+        <div className="category-list d-flex justify-content-center align-items-center" style={{ marginTop:"0%", marginBottom:"0%" }}>
+          {[
+            { name: "방향제/공기청정", link: "/category/air-fresheners" },
+            { name: "바닥/트렁크 매트", link: "/category/floor-mats" },
+            { name: "시트/쿠션", link: "/category/seat-cushions" },
+            { name: "커버/몰딩", link: "/category/covers-moldings" },
+            { name: "수납/정리용품", link: "/category/storage-organizers" },
+            { name: "편의용투/액세사리", link: "/category/convenience-accessories" },
+            { name: "햇빛가리개/썬팅", link: "/category/sunshades-tinting" },
+          ].map((category) => (
+              <a href={category.link} className="text-decoration-none" style={{ color: 'black', margin: '0 2%' }}>
+                {category.name}
+              </a>
+          ))}
+        </div>
+      </header>
+  );
+};
+
+export default Header;
