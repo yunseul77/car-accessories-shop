54c4f3ae428248ae1b7c68e4f59f4bfeb9ab21c1
[Cloud3] 박규태 <novico@naver.com>
2024-06-15 06:16:45 +0000

    Add new file

---
+++ src/main/java/com/team9/carshop/SercurityConfig.java
@@ -0,0 +1,20 @@
+package com.team9.carshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .anyRequest().permitAll()  // 모든 요청을 허용
+                .and()
+            .csrf().disable();  // CSRF 보호 비활성화 (개발 환경에서만 사용)
+    }
+}
